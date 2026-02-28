b90d7d7b68cd8e434db88e96d7e4ef81bd5c3d6a
Brayden Yan <235433919+braydenyan@users.noreply.github.com>
2026-02-27 22:44:19 -0500

    Update replaylist.py

--- replaylist.py
+++ replaylist.py
@@ -13,7 +13,7 @@ from ytmusicapi import YTMusic # type: ignore
 # =========================
 
 PLAYLIST_URL = "https://music.youtube.com/playlist?list=PUT_YOUR_PLAYLIST_ID_OR_URL_HERE"
-TOP_DOWN = True
+TOP_DOWN = False
 SLEEP_SECONDS = 1.5
 
 # insert "Authorization" and "Cookie" values from browser login session (do NOT share!)
@@ -198,6 +198,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
